736930985885983d701043eab7788413faa573f2
LACHICHITURBO\mmoya <miguelmmoya26@outlook.com>
2024-04-11 18:42:22 -0500

    Create home and recipient data review stats initial files

---
+++ src/app/recipient-data-review-stats/recipient-data-review-stats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipient-data-review-stats',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipient-data-review-stats.component.html',
+  styleUrl: './recipient-data-review-stats.component.css'
+})
+export class RecipientDataReviewStatsComponent {
+
+}
