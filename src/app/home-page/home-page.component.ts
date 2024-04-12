9379a6954e140a0939fcf20bfc9d5125fab509e6
LACHICHITURBO\mmoya <miguelmmoya26@outlook.com>
2024-04-12 00:21:00 -0500

    Start HTML & CSS for recipient data review table and stats component + add icon assets

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RecipientDataReviewStatsComponent } from '../recipient-data-review-stats/recipient-data-review-stats.component';
+import { RecipientDataReviewTableComponent } from '../recipient-data-review-table/recipient-data-review-table.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [RecipientDataReviewStatsComponent, RecipientDataReviewTableComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
---
+++ src/app/recipient-data-review-table/recipient-data-review-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipient-data-review-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipient-data-review-table.component.html',
+  styleUrl: './recipient-data-review-table.component.css'
+})
+export class RecipientDataReviewTableComponent {
+
+}
