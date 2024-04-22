718878e89f724a82a66c7135835f67486fff1d7b
mmoya26 <mmoya18@icloud.com>
2024-04-22 16:17:45 -0500

    Fix modal to show and hide and remove internal state from upload-forms-modal for visibiity

--- src/app/upload-forms-modal/upload-forms-modal.component.ts
+++ src/app/upload-forms-modal/upload-forms-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Output, EventEmitter, Input} from '@angular/core';
 import { DropdownModule } from 'primeng/dropdown';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
@@ -11,10 +11,13 @@ import { InputTextModule } from 'primeng/inputtext';
   styleUrl: './upload-forms-modal.component.css'
 })
 export class UploadFormsModalComponent {
-  @Input() isVisible : boolean = false
 
+  @Input() isVisible! : boolean
+
+  @Output() hideUploadForms = new EventEmitter<false>();
+  
   hide() {
-    this.isVisible = false
+    this.hideUploadForms.emit(false);
   }
 
   public formTypes: Object[] = [{formType: "1099-NEC"}, {formType: "1099-MISC"}, {formType: "1099-INT"}, {formType: "W-2"}];
