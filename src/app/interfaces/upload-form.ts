b83124505ad578c4eb660d5383175eed9e2ccb60
mmoya26 <mmoya18@icloud.com>
2024-04-25 00:50:09 -0500

    Created various methods to capture form values & created interface to represent the form being created

---
+++ src/app/interfaces/upload-form.ts
@@ -0,0 +1,7 @@
+export interface UploadForm {
+    formType: String,
+    taxYear: String,
+    isFileProductionType: boolean,
+    notes: String,
+    fileUploaded: String
+}
--- src/app/upload-forms-modal/upload-forms-modal.component.ts
+++ src/app/upload-forms-modal/upload-forms-modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, Output, EventEmitter, Input} from '@angular/core';
-import { DropdownModule } from 'primeng/dropdown';
+import { DropdownModule, DropdownChangeEvent } from 'primeng/dropdown';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { OverlayOptions, OverlayListenerOptions } from 'primeng/api';
 import { FormsModule } from '@angular/forms';
 import { FileUploadModule, FileUploadEvent } from 'primeng/fileupload';
+import { UploadForm } from '../interfaces/upload-form';
 
 @Component({
   selector: 'app-upload-forms-modal',
@@ -16,27 +17,61 @@ import { FileUploadModule, FileUploadEvent } from 'primeng/fileupload';
 export class UploadFormsModalComponent {
 
   @Input() isVisible! : boolean
-
   @Output() hideUploadForms = new EventEmitter<boolean>();
+  form = {} as UploadForm
 
-  notesField : String  = ''
+  handleFileUploaded({files} : FileUploadEvent) {
+    const file = files[0];
+    this.form.fileUploaded = file.name
 
-  onUpload(event : FileUploadEvent) {
+    console.log(this.form);
+  }
 
+  handleFileType(event: Event) {
+    const target = event.target as HTMLButtonElement;
+
+    if (target.innerHTML.toLocaleLowerCase() === 'production') {
+      this.form.isFileProductionType = true
+    } else {
+      this.form.isFileProductionType = false
+    }
+
+    console.log(this.form);
+   }
+
+  handleDropdown({value}: DropdownChangeEvent) {
+    if (value.fieldName === 'formType') {
+      this.form.formType = value.formType
+    } else if (value.fieldName === 'taxYear') {
+      this.form.taxYear = value.year
+    }
+
+    console.log(this.form);
   }
   
   hide() {
     this.hideUploadForms.emit(false);
+    this.form = {} as UploadForm;
+
+    console.log(this.form)
   }
 
   getOverlayOptions(): OverlayOptions {
     return {
       listener: (event: Event, options?: OverlayListenerOptions) => {
+        if (event.type === "click") {
+          return true
+        }
         return false;
       }
     };
 }
 
-  public formTypes: Object[] = [{formType: "1099-NEC"}, {formType: "1099-MISC"}, {formType: "1099-INT"}, {formType: "W-2"}];
-  public taxYear: Object[] = [{year: "2016"}, {year: "2017"}, {year: "2018"}, {year: "2019"}, {year: "2020"}, {year: "2021"}, {year: "2022"}, {year: "2023"},];
+  public formTypes: Object[] = [{fieldName: "formType", formType: "1099-NEC"}, {fieldName: "formType", formType: "1099-MISC"}, {fieldName: "formType", formType: "1099-INT"}, {fieldName: "formType", formType: "W-2"}];
+  public taxYear: Object[] = [
+    {fieldName: "taxYear", year: "2016"}, {fieldName: "taxYear", year: "2017"},
+    {fieldName: "taxYear", year: "2018"}, {fieldName: "taxYear", year: "2019"}, 
+    {fieldName: "taxYear", year: "2020"}, {fieldName: "taxYear", year: "2021"}, 
+    {fieldName: "taxYear", year: "2022"}, {fieldName: "taxYear", year: "2023"},
+  ];
 }
